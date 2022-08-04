88bb7c4803d42489dbc5b1eaab6c155cdfdc7f56
asangeethkumar <sangeethkumar.akkaraju@gmail.com>
2022-08-04 10:57:56 +0530

    Update printecho.sh

--- printecho.sh
+++ printecho.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+      curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+        ibmcloud --version
+        ibmcloud config --check-version=false
+        ibmcloud plugin install -f kubernetes-service
+        ibmcloud plugin install -f container-registry
+        
+          ibmcloud login -r eu-de --apikey "${IBM_CLOUD_API_KEY}"
+        ibmcloud cr region-set "${IBM_CLOUD_REGION}"
+        ibmcloud cr login
+        
 
 echo "printong inputs values env"
 echo $INPUT_TAGS
