f0f13e2debcd3fe9621b4335c5946783638bacc0
asangeethkumar <sangeethkumar.akkaraju@gmail.com>
2022-08-04 10:51:32 +0530

    Update printecho.sh

--- printecho.sh
+++ printecho.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Read named command line arguments into an args variable
+
+declare -A args
+
+while (( "$#" )); do
+    if  [[ $1 == --* ]] && [ "$2" ]; then
+        args[${1:2}]=$2
+        shift
+    fi
+    shift
+done
+
+
+
 echo "${args[tags]}"
 echo "next value is tag"
 echo ${args[loglevel]}"
